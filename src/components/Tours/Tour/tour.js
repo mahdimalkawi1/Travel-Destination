eecdab1e3c3f81f9f568d4b06d620a75f1f1da3c
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-04-04 19:45:04 +0000

    made some changes

---
+++ src/components/Tours/Tour/tour.js
@@ -0,0 +1,12 @@
+// import "./Tour.css";
+
+// function Tour(props) {
+//     return (
+//         <div>
+//             <h2>{props.name}</h2>
+//             <img src={props.image} alt={props.name} />
+//         </div>
+//     )
+// }
+
+// export default Tour;
