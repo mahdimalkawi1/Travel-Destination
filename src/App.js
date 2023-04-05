6280d44db6618951c7bc210de4a398ba0d278156
mahdimalkawi1 <mahdimalkawi66@gmail.com>
2023-04-05 00:01:43 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import TourDetails from "./Components/TourDetails/TourDetails";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 const ToursData = require("./data/db.json")
 
 function App() {
 
     return (
-        
-        <div>
-            <Home data={ToursData}/>
+        <Router>
+            <Navbar />
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home data={ToursData}/>}></Route>
+                <Route path="/city/:id" element={<TourDetails/>}></Route>
 
+            </Routes>
+            <Footer />
         </div>
+        </Router>
     )
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import "./Navbar";
+import { Link } from "react-router-dom";
+const Navbar =()=>{
+    return(
+        <nav>
+        <link to="/">Home</link>
+        </nav>
+    )
+}
+export default Navbar
